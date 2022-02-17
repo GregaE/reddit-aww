5175b660cf3a4f02cc59e9a5be3b660767ac9c76
gregae <grega.erzen@outlook.com>
2022-02-17 22:15:55 +0100

    style: header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="/assets/aww.png" alt="logo"></img>
       </header>
       <RedditListContainer/>
     </div>
