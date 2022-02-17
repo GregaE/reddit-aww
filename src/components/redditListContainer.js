8a46f4f2707f2878584ad0f8b284f1cb3fe0afb9
gregae <grega.erzen@outlook.com>
2022-02-17 20:55:32 +0100

    feat: render first 25 posts

--- src/components/redditListContainer.js
+++ src/components/redditListContainer.js
@@ -1,10 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
 import RedditListItem from "./redditListItem";
 import { getHotPosts } from "../services/redditApiService"
 
 function RedditListContainer() {
 
+  const [redditPostList, setRedditPostList] = useState([]);
+
+  function renderPosts(list) {
+    return list.map(post => {
+      return <RedditListItem
+          key={post.data.name}
+          title={post.data.title}
+          thumbnail={post.data.thumbnail}
+          path={post.data.permalink}
+          subreddit={post.data.subreddit_name_prefixed}
+        />
+      }
+    )
+  }
+
+  useEffect(()=> {
+    getHotPosts()
+      .then(res => {
+        setRedditPostList(res.data.children)
+      })
+      .catch(console.log('An error occurred when fetching Reddit posts'))
+  },[setRedditPostList])
+
+  console.log(redditPostList)
+
   return (
     <div className="RedditListContainer">
+      {renderPosts(redditPostList)}
     </div>
   );
 }
--- src/components/redditListItem.js
+++ src/components/redditListItem.js
@@ -1,6 +1,11 @@
-function RedditListItem() {
+function RedditListItem( { title, thumbnail, path, subreddit } ) {
   return (
     <div className="RedditListItem">
+      <h2>
+        <a href={`www.reddit.com${path}`}>{title}</a>
+      </h2>
+      <img src={thumbnail} alt="play-button" />
+      <div>{subreddit}</div>
     </div>
   );
 }
